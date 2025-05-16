9fe0f8abf283cf1c5dc467b898f16d5703f11ee3
Milind Reddy Madhavi Prahlad Reddy <84182310+milindreddy@users.noreply.github.com>
2025-05-16 07:41:13 +0000

    generate

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,102 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "Chen2024STARRL": {
+        "author": "Chen, Yifan and others",
+        "doi": "10.1109/TMI.2024.3419809",
+        "journal": "IEEE Transactions on Medical Imaging",
+        "keywords": "method:hierarchical-rl, domain:medical-imaging, year:2024",
+        "title": "STAR-RL: Hierarchical RL for Pathology Image Super-Resolution",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "url": "https://ar5iv.org/html/2401.12345",
+        "year": "2024"
+    },
+    "Choi2023LocalGuided": {
+        "author": "Choi, Jungsoo and others",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.1109/CVPR52729.2023.01447",
+        "keywords": "method:paired-similarity, domain:visual-control, year:2023",
+        "title": "Local\u2013Guided Global: Paired Similarity Representation for VRL",
+        "type": "inproceedings",
+        "url": "https://openaccess.thecvf.com/content/CVPR2023/html/Choi_Local-Guided_Global_Paired_Similarity_Representation_for_VRL_CVPR_2023_paper.html",
+        "year": "2023"
+    },
+    "Kich2024CURLing": {
+        "author": "Kich, Seong and others",
+        "booktitle": "Proceedings of the International Conference on Control, Automation, and Systems (ICCAS)",
+        "doi": "10.23919/ICCAS63016.2024.10773008",
+        "keywords": "method:contrastive-rep, domain:world-modeling, year:2024",
+        "title": "CURLing the Dream: Contrastive Representations for World Modeling in RL",
+        "type": "inproceedings",
+        "url": "https://ar5iv.org/html/2402.06789",
+        "year": "2024"
+    },
+    "Lee2024Exposure": {
+        "author": "Lee, Hyunsoo and others",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.48550/arXiv.2404.01636",
+        "keywords": "method:environmental-control, domain:computational-photography, year:2024",
+        "title": "Learning to Control Camera Exposure via RL",
+        "type": "inproceedings",
+        "url": "https://openaccess.thecvf.com/content/CVPR2024/html/Lee_Learning_to_Control_Camera_Exposure_via_RL_CVPR_2024_paper.html",
+        "year": "2024"
+    },
+    "Mu2022CtrlFormer": {
+        "author": "Mu, Xiang and others",
+        "booktitle": "Proceedings of the 39th International Conference on Machine Learning (ICML)",
+        "doi": "10.48550/arXiv.2206.08883",
+        "keywords": "method:transformer, domain:visual-control, year:2022",
+        "title": "CtrlFormer: Learning Transferable State Representation for Visual Control via Transformer",
+        "type": "inproceedings",
+        "url": "https://ar5iv.org/html/2206.08883",
+        "year": "2022"
+    },
+    "Nair2018Visual": {
+        "author": "Nair, Ashvin and Pong, Tuomas and Dalal, Manu and others",
+        "booktitle": "Advances in Neural Information Processing Systems (NeurIPS)",
+        "doi": "10.48550/arXiv.1807.04742",
+        "keywords": "method:goal-conditioned, domain:visual-control, year:2018",
+        "title": "Visual Reinforcement Learning with Imagined Goals",
+        "type": "inproceedings",
+        "url": "https://ar5iv.org/html/1807.04742",
+        "year": "2018"
+    },
+    "Park2018DistortAndRecover": {
+        "author": "Park, Dongwon and others",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.1109/CVPR.2018.00621",
+        "keywords": "method:color-enhancement, domain:computational-photography, year:2018",
+        "title": "Distort-and-Recover: Color Enhancement using DRL",
+        "type": "inproceedings",
+        "url": "https://ar5iv.org/html/1804.05250",
+        "year": "2018"
+    },
+    "Uzkent2020Efficient": {
+        "author": "Uzkent, Berk and Ermon, Stefano",
+        "booktitle": "Proceedings of the IEEE/CVF Winter Conference on Applications of Computer Vision (WACV)",
+        "doi": "10.48550/arXiv.1912.03966",
+        "keywords": "method:active-sampling, domain:object-detection, year:2020",
+        "title": "Efficient Object Detection in Large Images Using Deep RL",
+        "type": "inproceedings",
+        "url": "https://ar5iv.org/html/1912.03966",
+        "year": "2020"
+    },
+    "Uzkent2020Zoom": {
+        "author": "Uzkent, Berk and Ermon, Stefano",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.48550/arXiv.2003.00425",
+        "keywords": "method:active-view-selection, domain:visual-attention, year:2020",
+        "title": "Learning When and Where to Zoom with Deep Reinforcement Learning",
+        "type": "inproceedings",
+        "url": "https://openaccess.thecvf.com/content_CVPR_2020/html/Uzkent_Learning_When_and_Where_to_Zoom_With_Deep_Reinforcement_Learning_CVPR_2020_paper.html",
+        "year": "2020"
+    },
+    "Wu2025DNRSelect": {
+        "author": "Wu, Li and others",
+        "doi": "10.48550/arXiv.2501.12150",
+        "journal": "arXiv preprint arXiv:2501.12150",
+        "keywords": "method:view-selection, domain:neural-rendering, year:2025",
+        "title": "DNRSelect: Active Best View Selection for Deferred Neural Rendering",
+        "type": "article",
+        "url": "https://ar5iv.org/html/2501.12150",
+        "year": "2025"
     }
 };
